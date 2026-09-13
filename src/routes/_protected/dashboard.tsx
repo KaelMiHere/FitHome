d35d08365a0480a44af82dfd80f239a2147ba8d8
FitUser <fit@home.local>
2026-09-13 21:56:59 +0700

    fix dashboard route path

--- src/routes/_protected/dashboard.tsx
+++ src/routes/_protected/dashboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
-export const Route = createFileRoute('/dashboard')({
+export const Route = createFileRoute('/_protected/dashboard')({
   component: DashboardPage,
 })
 function DashboardPage() {
