50e5868309d2ba9543068888a81fc927f1c2b38a
FitUser <fit@home.local>
2026-09-13 22:24:51 +0700

    fix streak.tsx baris 2: tambah import React

--- src/routes/_protected/streak.tsx
+++ src/routes/_protected/streak.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { streakKeys } from '../../lib/query'
